40680c2d0bc72f50a2aaf3e10e8ff76258171815
keisukepomu <keisukepomu@gmail.com>
2017-02-25 08:29:59 +0900

    カレンダーちょっと進めた

--- GroupWork/public/javascripts/calendar.js
+++ GroupWork/public/javascripts/calendar.js
@@ -1,5 +1,29 @@
-var createTable = function(){
+var yyyy, mm, dd;
+var date = new Date();
+yyyy = date.getFullYear();
+mm = date.getMonth() + 1;
+dd = date.getDate();
 
+var createTable = function(){
+	var tdDay = new Array();
+
+	for (i = 0 ; i < 35 ; i++) {
+		tdDay[i] = document.createElement('td');
+		tdDay[i].innerText = i + 1;
+	}
+
+	var tmpCnt = 0;
+	var calendarBody = document.getElementsByClassName('calendarBody').item(0);
+	for (j = 0 ; j < 5 ; j++) {
+		var tr = document.createElement('tr');
+		for (i = 0 + tmpCnt ; i < 7 + tmpCnt ; i++) {
+			tr.appendChild(tdDay[i]);
+		}
+		tmpCnt = tmpCnt + 7
+		calendarBody.appendChild(tr);
+	}
+
+	document.getElementById('thisMonth').innerText = mm + "月";
 };
 
 window.onload = function(){
