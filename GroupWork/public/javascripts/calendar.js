a6d392e59126afba55726cf9e2e7d2f91d463e0a
keisukepomu <keisukepomu@gmail.com>
2017-02-26 19:33:21 +0900

    ちょっとカレンダー追加

--- GroupWork/public/javascripts/calendar.js
+++ GroupWork/public/javascripts/calendar.js
@@ -1,15 +1,30 @@
-var yyyy, mm, dd;
+var yyyy, mm, dd, initWeek;
 var date = new Date();
 yyyy = date.getFullYear();
 mm = date.getMonth() + 1;
 dd = date.getDate();
 
 var createTable = function(){
+	// 年月日情報取得
+	var tmpDate = date;
+	tmpDate.setDate(1);
+	// １日の曜日
+	initWeek = tmpDate.getDay();
+	tmpDate.setMonth(tmpDate.getMonth() + 1);
+	tmpDate.setDate(0);
+	// 今月の最終日
+	var lastDay = tmpDate.getDate();
+	//
+	var tblCnt = lastDay + initWeek;
+
+	// テーブル作成
 	var tdDay = new Array();
 
-	for (i = 0 ; i < 35 ; i++) {
+	for (i = 0 ; i < 42 ; i++) {
 		tdDay[i] = document.createElement('td');
-		tdDay[i].innerText = i + 1;
+		if (i >= initWeek && i < lastDay + initWeek) {
+			tdDay[i].innerText = i + 1 - initWeek;
+		}
 	}
 
 	var tmpCnt = 0;
